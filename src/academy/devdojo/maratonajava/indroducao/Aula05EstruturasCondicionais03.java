7716f1af39277fe125b135f3ef6e042ad8ce5271
LucasMenezes9 <llucas009@yahoo.com.br>
2025-02-22 20:45:58 -0300

    22/02/2025

--- src/academy/devdojo/maratonajava/indroducao/Aula05EstruturasCondicionais03.java
+++ src/academy/devdojo/maratonajava/indroducao/Aula05EstruturasCondicionais03.java
@@ -2,6 +2,19 @@ package academy.devdojo.maratonajava.indroducao;
 
 public class Aula05EstruturasCondicionais03 {
     public static void main(String[] args) {
-        
+        // Doar se salario for > 5000
+        double salario = 4000;
+        String mensagemDoar = "Eu vou doar R$ 600,00";
+        String mensagemNaoDoar = "Ainda não tenho condições, mas vou ter!";
+
+        // (condicao) ? verdadeiro : falso;
+        String resultado = salario > 5000 ? mensagemDoar : mensagemNaoDoar;
+
+        /* if (salario > 5000) {
+            resultado = mensagemDoar;
+        }else {
+            resultado = mensagemNaoDoar;   */
+
+        System.out.println(resultado);
     }
 }
